c4774dd59f1911aee315a60cd619902714417076
DilanKarayilan <74790204+DilanK1@users.noreply.github.com>
2023-10-10 17:51:16 +0300

    added acilirButton click action

--- navigation.js
+++ navigation.js
@@ -1,76 +1,73 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Sayfa tamamen yüklendiğinde bu kod çalışır.
-  
-    // Düğme ve bağlantıyı alın
-    var konum_button = document.getElementById("konum_button");
-    var konum_link = document.getElementById("konum_link");
-  
-    // Düğmeye tıklandığında bir olay dinleyici ekleyin
-    konum_button.addEventListener("click", function() {
-      // Bağlantıyı açmak için window.location.href kullanılır.
-      window.location.href = konum_link.getAttribute("href");
-    });
-// *************************************
-     var menu_button = document.getElementById("menu_button");
-   
-     menu_button.addEventListener("click", function() {
-       window.location.href = "menu.html"; // Yeni sayfanın URL'sini buraya ekleyin
-     });
-// **************************************
-    var instagram_button = document.getElementById("instagram_button");
-    var instagram_link = document.getElementById("instagram_link");
-  
-    instagram_button.addEventListener("click", function() {
-      window.location.href = instagram_link.getAttribute("href");
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Sayfa tamamen yüklendiğinde bu kod çalışır.
 
-    var acilirButton = document.getElementById("acilirButton");
-        var acilirIcerik = document.getElementById("acilirIcerik");
+  // Düğme ve bağlantıyı alın
+  var konum_button = document.getElementById("konum_button");
+  var konum_link = document.getElementById("konum_link");
 
-        acilirButton.addEventListener("click", function () {
-            if (acilirIcerik.style.display === "block") {
-                acilirIcerik.style.display = "none";
-            } else {
-                acilirIcerik.style.display = "block";
-            }
-        });
-        
-        // Dışa tıklamayı dinle
-        document.addEventListener('click', function (event) {
-          if (!acilirButton.contains(event.target)) {
-              // Açılır menüyü gizle
-              acilirIcerik.style.display = 'none';
-          }
-      });
-       // Düğmeye tıklandığında açılır menüyü göster
-       acilirButton.addEventListener('click', function (event) {
-        event.stopPropagation(); // Dışa tıklamayı engelle
-        dropdownContent.style.display = 'block';
-    });
-
-    var dropdown = document.querySelector('.acilir-menu');
-    var dropdownContent = document.querySelector('.acilir-icerik');
-
-    // Açılır menüyü yukarı doğru açma işlemi
-    function openDropdownUpward() {
-      var windowHeight = window.innerHeight;
-      var dropdownPosition = dropdown.getBoundingClientRect().top;
-
-      if (dropdownPosition + dropdownContent.offsetHeight > windowHeight) {
-          dropdownContent.style.top = 'auto';
-          dropdownContent.style.bottom = '100%';
-      }
-  }
-
-  // Düğmeye tıklandığında açılır menüyü yukarı doğru aç
-  dropdown.addEventListener('click', function() {
-      dropdownContent.style.opacity = '1';
-      dropdownContent.style.visibility = 'visible';
-      dropdownContent.style.transform = 'translateY(0)';
-      
-      // Açılır menüyü yukarı doğru açma işlemi
-      openDropdownUpward();
+  // Düğmeye tıklandığında bir olay dinleyici ekleyin
+  konum_button.addEventListener("click", function () {
+    // Bağlantıyı açmak için window.location.href kullanılır.
+    window.location.href = konum_link.getAttribute("href");
   });
-      
+  // *************************************
+  var menu_button = document.getElementById("menu_button");
+
+  menu_button.addEventListener("click", function () {
+    window.location.href = "menu.html"; // Yeni sayfanın URL'sini buraya ekleyin
   });
-  
+  // **************************************
+  var instagram_button = document.getElementById("instagram_button");
+  var instagram_link = document.getElementById("instagram_link");
+
+  instagram_button.addEventListener("click", function () {
+    window.location.href = instagram_link.getAttribute("href");
+  });
+
+// MENÜÜ******************
+
+// Düğmeleri ve içerikleri alın
+const openCloseButtons = document.querySelectorAll("[id^='acilirButton']");
+const openCloseContents = document.querySelectorAll("[id^='acilirIcerik']");
+
+let currentlyOpenMenu = null;
+
+// Her düğme için olay dinleyicisi ekleyin
+openCloseButtons.forEach((button, index) => {
+    button.addEventListener("click", (event) => {
+        event.stopPropagation(); // Düğme tıklaması menü dışındaki tıklamalara etki etmesin
+        const openCloseMenu = openCloseContents[index];
+
+        // Eğer zaten başka bir menü açıksa, onu kapat
+        if (currentlyOpenMenu && currentlyOpenMenu !== openCloseMenu) {
+            currentlyOpenMenu.style.opacity = "0";
+            currentlyOpenMenu.style.visibility = "hidden";
+        }
+
+        if (openCloseMenu.style.visibility === "hidden" || openCloseMenu.style.visibility === "") {
+            openCloseMenu.style.opacity = "1";
+            openCloseMenu.style.visibility = "visible";
+            currentlyOpenMenu = openCloseMenu; // Şu anki menüyü güncelle
+            // Menüyü açtığınızda, dışarıya tıklamalarda menünün kapanmasını dinleyin
+            document.addEventListener("click", closeMenuOnClickOutside);
+        } else {
+            openCloseMenu.style.opacity = "0";
+            openCloseMenu.style.visibility = "hidden";
+        }
+    });
+});
+
+// Menünün dışına tıklandığında çalışacak fonksiyon
+function closeMenuOnClickOutside(event) {
+    openCloseContents.forEach((menu) => {
+        if (!menu.contains(event.target)) {
+            menu.style.opacity = "0";
+            menu.style.visibility = "hidden";
+            currentlyOpenMenu = null; // Şu anki menüyü sıfırla
+            // Menüyü kapatın ve dışarıya tıklamalarda dinlemeyi kaldırın
+            document.removeEventListener("click", closeMenuOnClickOutside);
+        }
+    });
+}
+ 
+});
